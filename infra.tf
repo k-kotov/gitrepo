a9cd206647b3b62448ff9462f8dfb8a29e5ae798
Nivall <38981440+k-kotov@users.noreply.github.com>
2020-06-11 09:02:44 +0300

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -257,7 +257,7 @@ resource "aws_rds_cluster_instance" "k3s" {
 #############################
 resource "aws_route53_record" "rancher" {
   count    = local.install_rancher ? local.create_external_nlb : 0
-  zone_id  = data.aws_route53_zone.dns_zone.zone_id
+  #zone_id  = data.aws_route53_zone.dns_zone.zone_id
   name     = "${local.name}.${local.domain}"
   type     = "CNAME"
   ttl      = 30
